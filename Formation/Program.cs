0d69d43ee06eba50bdb45bf0705084f329c14682
Formation <formation@intm.com>
2024-11-06 18:01:55 +0100

    Partie 2 exercice 1 fait

--- Formation/Program.cs
+++ Formation/Program.cs
@@ -15,12 +15,16 @@ namespace Formation
             BasicOperation(3, 0, '/');
             BasicOperation(6, 9, 'L');
 
+            IntegerDivision(12, 4);
+            IntegerDivision(13, 4);
+            IntegerDivision(12, 0);
+
             Console.ReadKey();
         }
-        static void BasicOperation (int a, int b, char operateur)
+        static void BasicOperation(int a, int b, char operateur)
         {
             int resultat;
-           //operateur = '/','*','-','+' ;
+            //operateur = '/','*','-','+' ;
 
             switch (operateur)
             {
@@ -40,28 +44,50 @@ namespace Formation
                     Console.WriteLine(a + " * " + b + " = " + resultat + " ");
                     break;
                 case '-':
-                    resultat =  a - b;
+                    resultat = a - b;
                     Console.WriteLine(a + " - " + b + " = " + resultat + " ");
                     break;
                 case '+':
-                    resultat =  a + b;
+                    resultat = a + b;
                     Console.WriteLine(a + " + " + b + " = " + resultat + " ");
                     break;
-                default:              
-                    Console.WriteLine(a + " " + operateur + " " + b + " = Opération Invalide .");
-                    break;     
+                default:
+                    Console.WriteLine(a + " " + operateur + " " + b + "= Opération Invalide .");
+
+                    break;
             }
 
         }
-        
-             
+        static void IntegerDivision(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("a" + "/" + "b" + "= Opération Invalide .");
+            }
+
+            else
+
+            {
+                int q = a / b;
+                int r = a % b;
+
+                if (r == 0)
+
+                {
+                    a = q * b + r;
+
+                    Console.WriteLine(a + " = " + q + " * " + b + " ");
+                }
+
+                else
+                {
+                    a = q * b + r;
+
+                    Console.WriteLine(a + " = " + q + " * " + b + " + " + r + " ");
+                }
 
 
-
-
-
-              
-
-
-    }
+            }
+        }
+    }        
 } 
