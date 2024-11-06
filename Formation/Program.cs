f2e3a5a61b4a30e398db79123f98d2cc47884514
Formation <formation@intm.com>
2024-11-06 16:51:02 +0100

    Exercice 1 realisé

--- Formation/Program.cs
+++ Formation/Program.cs
@@ -8,8 +8,51 @@ namespace Formation
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            BasicOperation(3, 4, '+');
+            BasicOperation(6, 2, '/');
+            BasicOperation(3, 0, '/');
+            BasicOperation(6, 9, 'L');
+
+            Console.ReadKey();
         }
+        static void BasicOperation (int a, int b, char operateur)
+        {
+            int resultat;
+           //operateur = '/','*','-','+' ;
+
+            switch (operateur)
+            {
+                case '/':
+                    if (b != 0)
+                    {
+                        resultat = a / b;
+                        Console.WriteLine(a + " / " + b + " = " + resultat + " ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(a + " / " + b + " = Opération Invalide .");
+                    }
+                    break;
+                case '*':
+                    resultat = a * b;
+                    Console.WriteLine(a + " * " + b + " = " + resultat + " ");
+                    break;
+                case '-':
+                    resultat =  a - b;
+                    Console.WriteLine(a + " - " + b + " = " + resultat + " ");
+                    break;
+                case '+':
+                    resultat =  a + b;
+                    Console.WriteLine(a + " + " + b + " = " + resultat + " ");
+                    break;
+                default:              
+                    Console.WriteLine(a + " " + operateur + " " + b + " = Opération Invalide .");
+                    break;     
+            }
+
+        }    
+
     }
-}
+} 
